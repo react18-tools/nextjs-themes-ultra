3edd065a0552360720e51bc3cc8899134e068305
Mayank <mayank.srmu@gmail.com>
2024-02-23 06:35:30 +0000

    test color switch

--- lib/nthul/src/client/color-switch/color-switch.test.tsx
+++ lib/nthul/src/client/color-switch/color-switch.test.tsx
@@ -1,12 +1,22 @@
-import { cleanup, render, screen } from "@testing-library/react";
-import { afterEach, describe, test } from "vitest";
+import { act, cleanup, fireEvent, render, renderHook, screen } from "@testing-library/react";
 import { ColorSwitch } from "./color-switch";
+import { afterEach, describe, test } from "vitest";
+import { useTheme } from "../../hooks/use-theme";
 
-describe.concurrent("color-switch", () => {
+describe("color-switch", () => {
   afterEach(cleanup);
 
-  test("check if h1 heading exists", ({ expect }) => {
+  test("color-scheme-toggle", async ({ expect }) => {
+    const hook = renderHook(() => useTheme());
     render(<ColorSwitch />);
-    expect(screen.getByTestId("color-switch-h1").textContent).toBe("color-switch");
+    const element = screen.getByTestId("color-switch");
+    await act(() => fireEvent.click(element));
+    expect(hook.result.current.colorSchemePreference).toBe("dark");
+    await act(() => fireEvent.click(element));
+    expect(hook.result.current.colorSchemePreference).toBe("light");
+    await act(() => fireEvent.click(element));
+    expect(hook.result.current.colorSchemePreference).toBe("system");
+    await act(() => fireEvent.click(element));
+    expect(hook.result.current.colorSchemePreference).toBe("dark");
   });
 });
